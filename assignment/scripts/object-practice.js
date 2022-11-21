029da38197bcddd7bf59281837b1b8575659b4de
Natalie McMann <nmcmann110@gmail.com>
2022-11-21 12:06:37 -0600

    completed tasks 4 & 5

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -40,7 +40,9 @@ console.log(me.favThreeFoods[2]);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
+console.log(me.shoeCount);
+me.shoeCount = me.shoeCount + 1;
+console.log(me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -48,3 +50,5 @@ console.log(me.favThreeFoods[2]);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = 'lavender';
+console.log(me);
